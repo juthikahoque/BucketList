d52f69757d9f9fad3d05bf3c4369c46ffb2a2edc
juthikahoque <77185514+juthikahoque@users.noreply.github.com>
2021-04-03 02:46:37 -0400

    Update MyList.js

--- src/MyList.js
+++ src/MyList.js
@@ -1,8 +1,11 @@
 import useFetch from './useFetch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useParams } from 'react-router-dom';
 
 const List = () => {
+    const { id } = useParams();
     const { data: mylist, isPending, error} = useFetch('http://localhost:8001/mylist');
-
+    
     return (
         <div className="mylist">
             { error && <div>{ error }</div>}
@@ -12,7 +15,18 @@ const List = () => {
                     <h1>My BucketList</h1>
                     { mylist.map((mylist) => (
                         <div className="preview" key={mylist.id}>
-                            <h2>{ mylist.title }</h2>
+                            <div className="activityname">
+                                <h2>{ mylist.title }</h2>
+                                <button className="contenticon" onClick={() => {
+                                    fetch('http://localhost:8001/mylist/' + mylist.id, {
+                                        method: 'DELETE'
+                                    }).then(() => {
+                                        window.location.reload();
+                                    })
+                                }}>
+                                    <DeleteIcon />
+                                </button>
+                            </div>
                             <div className="location">
                                 <h4>Location: </h4>
                                 <p> { mylist.location }</p>
